48d761e097696af347410b15b69886bd209a7594
yogevgit10 <154273369+yogevgit10@users.noreply.github.com>
2024-01-01 21:16:56 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -11,7 +11,7 @@ public class DamkaBoard
 			if (i % 2 ==1 ) System.out.print(" ");
 			for (int j = 0 ; j <= n-1 ; j++)
 			{
-				if (j == (n-1)) System.out.print("*");
+				if ((i % 2 == 1) && (j == (n-1))) System.out.print("*");
 				else System.out.print("* "); 
 			}
 		    System.out.println("");
