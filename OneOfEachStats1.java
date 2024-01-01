c1a2d4d655d30416ccc1ae508198404cbd749b04
yogevgit10 <154273369+yogevgit10@users.noreply.github.com>
2024-01-01 19:48:14 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -4,8 +4,51 @@
  *  The program expects to get one command-line argument: an int value
  * 	that determines how many families to simulate.
  */
-public class OneOfEachStats1 {
-	public static void main (String[] args) {
-		//// Put your code here
+public class OneOfEachStats1
+
+{
+	public static void main(String[] args) 
+	{
+		int T = Integer.parseInt(args[0]); 
+		int boys = 0;
+		int girls = 0;
+		double sum = 0;
+		double random = 0.5;
+		int family2= 0;
+        int family3= 0;
+        int family4ormore= 0;
+		for (int i = 1 ; i <= T ; i++)
+		{
+            while ( boys == 0 || girls == 0)
+		{
+			random = Math.random();
+			if (random < 0.5)
+			{
+				boys++;
+				//System.out.print("b ");
+			}
+			else 
+			{
+				girls++;
+				//System.out.print("g ");	
+			}
+			
+		}
+		if ( (girls + boys) == 2 ) family2 = family2 + 1;
+		else if ((girls + boys) == 3 ) family3 = family3 + 1;
+		else family4ormore = family4ormore + 1;
+		sum = sum + girls + boys;
+		girls = 0;
+		boys = 0;
+		}
+		System.out.println("Avarage: " + (sum / ((double) (T))) + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + family2);
+        System.out.println("Number of families with 3 children: " + family3);
+		System.out.println("Number of families with 4 or more children: " + family4ormore);
+        System.out.println("The most common number of children is " + Math.max(Math.max(family2, family3),family4ormore));
+        //System.out.println("You made it... and you now have " + (girls + boys) + " children");
+
+
+
 	}
 }
